56072df51609df756c21592dc7f00b0b826ad488
luthfiyyah hanifah amari <luthfiyyah.205025@gmail.com>
2023-12-20 17:19:48 +0700

    feat: add task 1 - hitung luas

---
+++ hitungLuas/app/src/main/java/com/ppb/hitungluas/MainActivity.java
@@ -0,0 +1,67 @@
+package com.ppb.hitungluas;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtPanjang, edtLebar;
+    private Button btnHitung;
+    private TextView txtLuas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Di dalam metode onCreate() pada MainActivity
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Hitung Luas Persegi Panjang");
+        }
+
+        edtPanjang = (EditText) findViewById(R.id.edt_panjang);
+        edtLebar = (EditText) findViewById(R.id.edt_lebar);
+        btnHitung = (Button) findViewById(R.id.btn_hitung);
+        txtLuas = (TextView) findViewById(R.id.txt_luas);
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String panjang = edtPanjang.getText().toString().trim();
+                String lebar = edtLebar.getText().toString().trim();
+
+                double p = Double.parseDouble(panjang);
+                double l = Double.parseDouble(lebar);
+
+                double luas = p * l;
+
+                txtLuas.setText("Luas : " + luas);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings) {
+            return true;
+        }
+        return false;
+    }
+}
